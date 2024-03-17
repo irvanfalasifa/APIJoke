4b32934810031a311cb415f438963d81dba29213
unknown <irvan.falasifa@gmail.com>
2024-03-17 15:11:39 +0700

    update text

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
+const buttons = document.querySelectorAll('.btn')
 const jokeEl = document.getElementById('joke')
 const jokeBtn = document.getElementById('jokeBtn')
 
 jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
+
+buttons.forEach(button => {
+  button.addEventListener('click', function (e) {
+      const x = e.pageX
+      const y = e.pageY
+
+      const buttonTop = e.target.offsetTop
+      const buttonLeft = e.target.offsetLeft
+
+      const xInside = x - buttonLeft
+      const yInside = y - buttonTop
+
+      const circle = document.createElement('span')
+      circle.classList.add('circle')
+      circle.style.top = yInside + 'px'
+      circle.style.left = xInside + 'px'
+
+      this.appendChild(circle)
+
+      setTimeout(() => circle.remove(), 500)
+  })
+})
 
 // USING ASYNC/AWAIT
 async function generateJoke() {
